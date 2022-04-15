40767f7fbc6c1d2df364f9b8fceb8ff33450e577
Alexey Galashko <82465083+Alex-Gal@users.noreply.github.com>
2022-04-15 16:06:05 +0300

    add slider

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="container">
+        <footer className='nav'>
+          <p className='nav__par'>МТС +375292060068</p>
+        </footer>
+      </div>
+    </div>
   )
 }
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel'
+import "../../../node_modules/pure-react-carousel/dist/react-carousel.es.css";
 
 import './MainPage.scss'
 
@@ -8,45 +10,12 @@ export const MainPage = () => {
   return (
     <>
       <Header />
+
       <main className='main'>
+
         <section className='about'>
-          <div className="container">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Distinctio esse voluptas excepturi earum repudiandae ipsa,
-              a culpa temporibus explicabo perspiciatis suscipit doloremque
-              quod fuga laboriosam quam inventore odio adipisci rem.</p>
+          <div className='container'>
+            <h2>About me</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Distinctio esse voluptas excepturi earum repudiandae ipsa,
               a culpa temporibus explicabo perspiciatis suscipit doloremque
@@ -57,7 +26,43 @@ export const MainPage = () => {
               quod fuga laboriosam quam inventore odio adipisci rem.</p>
           </div>
         </section>
+
+        <section className='galary'>
+          <div className='container'>
+            <CarouselProvider
+              naturalSlideWidth={100}
+              naturalSlideHeight={125}
+              totalSlides={6}
+              isPlaying={true}
+              interval={3000}
+              touchEnabled={true}
+              visibleSlides={3}
+              infinite={true}
+            >
+              <Slider className='slider-box'>
+                <Slide index={0}>I am the first Slide.</Slide>
+                <Slide index={1}>I am the second Slide.</Slide>
+                <Slide index={2}>I am the third Slide.</Slide>
+                <Slide index={3}>I am the 4 Slide.</Slide>
+                <Slide index={4}>I am the 5 Slide.</Slide>
+                <Slide index={5}>I am the 6 Slide.</Slide>
+              </Slider>
+              <div className='slider-buttons'>
+                <ButtonBack>Back</ButtonBack>
+                <ButtonNext>Next</ButtonNext>
+                <DotGroup />
+              </div>
+            </CarouselProvider>
+          </div>
+        </section>
+
+        <section className='promo'>
+          <div className='container'>
+            Promo Video
+          </div>
+        </section>
       </main>
+
       <Footer />
     </>
   )
