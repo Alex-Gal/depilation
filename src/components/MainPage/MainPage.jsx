66a3ab1d1c85211e42a082c3968a894aed6f35e0
Alex-Gal <galashkoa@gmail.com>
2022-04-16 17:03:42 +0300

    edit slider and add arrows

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel'
-import "../../../node_modules/pure-react-carousel/dist/react-carousel.es.css";
+import {sliderImagesArr} from '../../img/sliderImages.js'
+import arrow from '../../icons/arrow.png';
 
+import "../../../node_modules/pure-react-carousel/dist/react-carousel.es.css";
 import './MainPage.scss'
 
 export const MainPage = () => {
+  const [img1, img2, img3, img4, img5, img6] = sliderImagesArr;
+
   return (
     <>
       <Header />
@@ -30,28 +34,31 @@ export const MainPage = () => {
         <section className='galary'>
           <div className='container'>
             <CarouselProvider
+              className='carousel-provider'
               naturalSlideWidth={100}
               naturalSlideHeight={125}
               totalSlides={6}
               isPlaying={true}
-              interval={3000}
+              interval={2000}
               touchEnabled={true}
               visibleSlides={3}
               infinite={true}
             >
               <Slider className='slider-box'>
-                <Slide index={0}>I am the first Slide.</Slide>
-                <Slide index={1}>I am the second Slide.</Slide>
-                <Slide index={2}>I am the third Slide.</Slide>
-                <Slide index={3}>I am the 4 Slide.</Slide>
-                <Slide index={4}>I am the 5 Slide.</Slide>
-                <Slide index={5}>I am the 6 Slide.</Slide>
+                <Slide className='slider-box__slide' index={0}><img src={img1} alt='Waxing'/></Slide>
+                <Slide className='slider-box__slide' index={1}><img src={img2} alt='Waxing'/></Slide>
+                <Slide className='slider-box__slide' index={2}><img src={img3} alt='Waxing'/></Slide>
+                <Slide className='slider-box__slide' index={3}><img src={img4} alt='Waxing'/></Slide>
+                <Slide className='slider-box__slide' index={4}><img src={img5} alt='Waxing'/></Slide>
+                <Slide className='slider-box__slide' index={5}><img src={img6} alt='Waxing'/></Slide>
               </Slider>
               <div className='slider-buttons'>
-                <ButtonBack>Back</ButtonBack>
-                <ButtonNext>Next</ButtonNext>
-                <DotGroup />
+                <ButtonBack className='slider-buttons__button'><img className='arrow-left' src={arrow} alt='Waxing'/></ButtonBack>
+                <ButtonNext className='slider-buttons__button'><img className='arrow-right' src={arrow} alt='Waxing'/></ButtonNext>
+                {/* <ButtonBack className='slider-buttons__button'><img src={arrow} alt='arrow' className="arrow-left"/></ButtonBack>
+                <ButtonNext className='slider-buttons__button'><img src={arrow} alt='arrow' className="arrow-right"/></ButtonNext> */}
               </div>
+              <DotGroup className='slider-dots' />
             </CarouselProvider>
           </div>
         </section>
